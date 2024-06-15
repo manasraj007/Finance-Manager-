d23585c1736ee40d57d1d91ccbdcaaa7ffc3e7bb
manasraj007 <h.manas@iitg.ac.in>
2024-06-15 21:26:46 +0000

    Added Category files

---
+++ Category.cpp
@@ -0,0 +1,11 @@
+#include "Category.h"
+
+Category::Category(const string& name) : name(name) {}
+
+string Category::getName() const {
+    return name;
+}
+
+void Category::setName(const string& name) {
+    this->name = name;
+}
