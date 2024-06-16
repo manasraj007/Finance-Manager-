c487374d77bc3d13f3977b782d855583fb3e2e0f
manasraj007 <h.manas@iitg.ac.in>
2024-06-16 18:38:26 +0000

    Deleted Income and Expense file and updated the json file

--- Expense.cpp
+++
@@ -1,9 +0,0 @@
-#include "Expense.h"
-
-Expense::Expense(const string& desc, double amt, time_t dt, const Category& cat)
-    : Transaction(desc, amt, dt, cat) {}
-
-void Expense::display() const {
-    cout << "Expense -> ";
-    Transaction::display();
-}
--- Income.cpp
+++
@@ -1,9 +0,0 @@
-#include "Income.h"
-
-Income::Income(const std::string& desc, double amt, time_t dt, const Category& cat)
-    : Transaction(desc, amt, dt, cat) {}
-
-void Income::display() const {
-    std::cout << "Income: ";
-    Transaction::display();
-}
